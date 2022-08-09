2d0bfceba3bba55820652a90ab214dee47e4b867
ashaduzzaman2002 <87015301+ashaduzzaman2002@users.noreply.github.com>
2022-08-09 13:36:25 +0530

    Update more functions

--- script.js
+++ script.js
@@ -4,66 +4,92 @@ const result = document.getElementById('result')
 const clearBtn = document.getElementById('clearBtn')
 const uppercase = document.getElementById('uppercase')
 const lowercase = document.getElementById('lowercase')
+const formatBtn = document.getElementById('formatBtn')
+const copyBtn = document.getElementById('copyBtn')
+const findLenghth = (item) => item==null? 0: item.length;
 
+
+//character Counter function
+const characterCounter = () => {
+  text = textInput.value
+  characters = findLenghth(text)
+  return characters
+}
 
 // Word counter Function
 const wordCounter = () => {
-    totalWord = 0
-    words = textInput.value.split(' ')
-    for (word of words){
-        totalWord++
-    }
-
-    return totalWord
+    text = textInput.value
+    word = findLenghth(text.match(/[a-zA-Z\d]+/g))
+    return word
 }
 
-//Leter Counter function
+// Sentence list
+const sentenceList = () => {
+  text = textInput.value
+  sentences = text.split('\.')
+  const i = sentences[sentences.length-1]
+  if(i == '' || i == ' '.repeat(i.length)){
+    sentences.pop()
+  }
+  return sentences
+}
 
-const leterCounter = () => {
-    totalLeter = 0
-    leters = textInput.value
-    for (leter of leters){
-        if(leter !== ' '){
-            totalLeter++
-        }
-    }
+// Sentence counter
+const sentenceCounter = () => {
+  sentence = findLenghth(sentenceList())
+  return sentence
+}
 
-    return totalLeter
+//Digit counter function
+const digitCounter = () => {
+  text = textInput.value
+  digit = findLenghth(text.match(/[\d]/g))
+  return digit
+}
+
+// Vowel counter function 
+const vowelConter = () => {
+  text = textInput.value
+  vowel = findLenghth(text.match(/[aeiou]/gi))
+  return vowel
+}
+
+// Consonant counter function 
+const consonantConter = () => {
+  text = textInput.value
+  consonant = findLenghth(text.match(/[b-df-hj-np-tv-z]/gi))
+  return consonant
 }
 
 // Text Analyze function
 
 analyzeBtn.onclick = () => {
-    result.innerHTML = `
-  <div>
-    <h2>${leterCounter()}</h2>
-    <h4>Character</h4>
-  </div>
+  let data1 = {
+    character: characterCounter(),
+    word: wordCounter(),
+    sentence: sentenceCounter(),
+    digit: digitCounter(),
+    vowel: vowelConter(),
+    consonant: consonantConter(),
+  }
 
-  <div>
-    <h2>${wordCounter()}</h2>
-    <h4>Word</h4>
-  </div>
-
-  <div>
-    <h2>0</h2>
-    <h4>Sentence</h4>
-  </div>
-
-  <div>
-    <h2>0</h2>
-    <h4>Digit</h4>
-  </div>
-
-  <div>
-    <h2>0</h2>
-    <h4>vowel</h4>
-  </div>
-
-  <div>
-    <h2>0</h2>
-    <h4>consonant</h4>
-  </div>`
+  if(textInput.value !== ''){
+  result.innerHTML = `<div id="lds-ring"><div></div><div></div><div></div><div></div></div>`
+  setTimeout(()=> {
+    result.innerHTML = ''
+    for(item in data1){
+      result.innerHTML += `
+      <div class="resultCard">
+      <h2>${data1[item]}</h2>
+      <h4>${item}</h4>
+      </div>`
+    }
+    
+}, 500)
+  }
+  else{
+    result.innerHTML = ''
+  }
 }
 
 // Clear function
@@ -79,8 +105,29 @@ uppercase.onclick = () => {
     textInput.value = uppercase
 }
 
+// Lowercase function
 lowercase.onclick = () => {
     let lowercase = textInput.value.toLowerCase()
     textInput.value = lowercase
 }
 
+// Copy text function
+copyBtn.onclick = () => {
+  textInput.select();
+  textInput.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(textInput.value)
+  navigator.clipboard.writeText(textInput.value);
+  
+  // change button text 
+  if(textInput.value !== ''){
+    copyBtn.innerText = 'copied'
+    setTimeout(() => copyBtn.innerText = 'copy text', 1000)
+  }
+}
+
+// format text button function
+formatBtn.onclick = () => {
+  text = textInput.value
+  newText = text.replace(/\s+/g,' ')
+  textInput.value = newText
+}
